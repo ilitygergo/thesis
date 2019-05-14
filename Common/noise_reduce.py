cc0aad106a0f22537abb39eab20736686a69e736
Ility Gergő <h659367@stud.u-szeged.hu>
2019-05-14 22:31:24 +0200

    Last commit hopefully

---
+++ Common/noise_reduce.py
@@ -0,0 +1,20 @@
+import cv2
+from Common.functions import imreadgray
+from Common.functions import flip
+
+picture = 'chromosomemini'
+
+# img = imreadgray('../DyerRosenfeld_1977/results/' + picture + '.png')
+img = imreadgray('../Kang_et_al/results/' + picture + '.png')
+img = imreadgray(picture + '.png')
+img = flip(img)
+size = img.shape
+
+for row in range(0, size[0]):
+    for col in range(0, size[1]):
+        if img[row][col] < 50:
+            img[row][col] = 0
+
+flip(img)
+
+cv2.imwrite('noise_reduced/' + picture + '.png', img)
