af4561e4866c44ffebfbfbd1da84888ac61ed3fc
ility.gergo <ility.gergo@innomedio.hu>
2020-12-16 20:06:27 +0100

    Deleting unnecessary chunks

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
-import time
 import tkinter as tk
 from common import Application
-from common import Image
-
-# startTime = time.time()
 
 try:
     root = tk.Tk()
@@ -11,6 +7,3 @@ try:
     app.mainloop()
 except Exception as e:
     print(e)
-
-# endTime = time.time()
-# print(endTime - startTime)
