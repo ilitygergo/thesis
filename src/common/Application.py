9f7b19221b3b1358d45e1a6912f027bde1bc4e5a
ility.gergo <ility.gergo@innomedio.hu>
2021-01-03 12:46:03 +0100

    Image positioning

--- src/common/Application.py
+++ src/common/Application.py
@@ -6,8 +6,8 @@ from .Image import Image
 
 class Application(tk.Frame):
     title = 'Thesis - Grayscale image thinning'
-    width = 700
-    height = 400
+    width = 600
+    height = 350
     menu = tk.Menu
     imageSrcLabel = tk.Label
     imageSizeLabel = tk.Label
@@ -19,6 +19,8 @@ class Application(tk.Frame):
         master.title(self.title)
         master.minsize(width=self.width, height=self.height)
         master.maxsize(width=self.width, height=self.height)
+        master.columnconfigure(0, weight=1)
+        master.rowconfigure(0, weight=1)
         self.displayMenu()
         self.displayImage()
         self.displayImageDetails()
@@ -43,19 +45,19 @@ class Application(tk.Frame):
         img = Image.getInstance()
         if img is None:
             self.canvas = tk.Canvas(self.master)
-            self.canvas.pack()
+            self.canvas.grid(row=0, column=0)
         else:
             self.photo = tk.PhotoImage(file=img.path)
             self.canvas.config(width=img.colSize, height=img.rowSize)
-            self.canvas.create_image(0, 0, image=self.photo)
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
     def displayImageDetails(self):
         img = Image.getInstance()
         if img is None:
             self.imageSrcLabel = tk.Label()
-            self.imageSrcLabel.pack()
+            self.imageSrcLabel.grid(row=1, column=0)
             self.imageSizeLabel = tk.Label()
-            self.imageSizeLabel.pack()
+            self.imageSizeLabel.grid(row=2, column=0)
         else:
             self.imageSrcLabel.config(text=img.name)
             sizeString = str(img.rowSize) + ' x ' + str(img.colSize)
